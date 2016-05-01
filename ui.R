762198a884b3c85c2be8cb01d63b1dcbe1b3a14a
Christopher Knutson <chris@dwolla.com>
2016-04-30 19:44:38 -0500

    Add instructions to side panel for using the application

--- ui.R
+++ ui.R
@@ -3,6 +3,13 @@ library(shiny)
 pageHeader <- h2("Regression to the Mean in Children's heights", align = "center")
 
 pageSidePanel <- sidebarPanel(
+    h3("This application predicts a child's height based on an input parent's height"),
+    p("The prediction is based on a linear model fit from the Galton's Height Data set"),
+    p("Select a parent's height using the slider below"),
+    p(paste("You will receive a prediction of the child's height, as well as a histogram", 
+            "the mean childrens' heights from Galton, the as well as the input parent's height,",
+            "illustrating how childrens' heights tend to regress towards the mean childrens'", "
+            height")),
     sliderInput('height', 'Parent Height, inches', value = 65, min = 60, max = 75, step = .1)
 )
 
